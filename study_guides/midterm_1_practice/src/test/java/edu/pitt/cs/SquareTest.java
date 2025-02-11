84b7f035e7916579144a58b67a06eead1eff6065
Ram Adesara <RKA22@pitt.edu>
2025-02-11 14:10:31 -0500

    Complete integration and unit tests for setSquared

--- study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/SquareTest.java
+++ study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/SquareTest.java
@@ -19,8 +19,10 @@ public class SquareTest {
 	 */
 	@Test
 	public void testSetSquaredIntegration() {
-		// TODO: Fill in!
-		fail();
+		Square square = new Square();
+		Number number = new Number();
+		square.setSquared(number, 3);
+		assertEquals(9, number.getVal());
 	}
 
 	/**
@@ -35,7 +37,9 @@ public class SquareTest {
 	 */
 	@Test
 	public void testSetSquaredUnit() {
-		// TODO: Fill in!
-		fail();
+		Square square = new Square();
+		Number number = Mockito.mock(Number.class);
+		square.setSquared(number, 3);
+		Mockito.verify(number).setVal(9);
 	}
 }
