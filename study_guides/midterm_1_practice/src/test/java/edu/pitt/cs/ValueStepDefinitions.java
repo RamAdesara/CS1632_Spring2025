b0c25b5e04491fa204b44af692e921175078db9a
Ram Adesara <RKA22@pitt.edu>
2025-02-13 09:22:56 -0500

    Complete value_incVal.feature and ValueStepDefinitions.java and create two new files

--- study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/ValueStepDefinitions.java
+++ study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/ValueStepDefinitions.java
@@ -6,5 +6,23 @@ import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
 
 public class ValueStepDefinitions {
-	// TODO: Fill in!
+	Value value;
+
+	@Given("a Value")
+	public void aValue() {
+		value = new Value();
+	}
+
+	@When("I call incVal {int} time(s)")
+	public void iCallIncValTimes(int n) {
+		for (int i = 0; i < n; i++) {
+			value.incVal();
+		}
+	}
+
+	@Then("getVal returns {int}")
+	public void getValReturns(int n) {
+		int ret = value.getVal();
+		assertEquals(n, ret);
+	}
 }
